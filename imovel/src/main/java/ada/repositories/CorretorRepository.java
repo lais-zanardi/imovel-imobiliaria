cce32c366c0b1e1e935abd74aa695d8674420d52
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-03-05 18:58:14 -0300

    add corretor repository

---
+++ imovel/src/main/java/ada/repositories/CorretorRepository.java
@@ -0,0 +1,7 @@
+package ada.repositories;
+
+import ada.entities.Corretor;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CorretorRepository extends CrudRepository<Corretor, String> {
+}
