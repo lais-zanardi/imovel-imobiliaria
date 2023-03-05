fa26cf79ba537cb76a99dec68d63a89721ac1deb
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-03-05 18:57:56 -0300

    adicionada corretor entity

--- imovel/src/main/java/ada/entities/Corretor.java
+++ imovel/src/main/java/ada/entities/Corretor.java
@@ -1,13 +1,19 @@
 package ada.entities;
 
 import lombok.Data;
+import lombok.Generated;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class Corretor {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
     private String nome;
+    private double taxaComissao;
 }
---
+++ imovel/src/main/java/ada/services/CorretorService.java
@@ -0,0 +1,34 @@
+package ada.services;
+
+import ada.entities.Corretor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CorretorService {
+    private final CrudRepository<Corretor, String> repository;
+
+    public Corretor save(Corretor entity) {
+        return repository.save(entity);
+    }
+
+    public Corretor atualizarCorretor(Corretor entity) {
+        Corretor atualizaCorretor = new Corretor();
+        atualizaCorretor.setId(entity.getId());
+        atualizaCorretor.setNome(entity.getNome());
+        atualizaCorretor.setTaxaComissao(entity.getTaxaComissao());
+        return atualizaCorretor;
+    }
+
+    public Optional<Corretor> getById(Corretor corretor) {
+        return repository.findById(corretor.getId());
+    }
+
+    public void deleteById(Corretor corretor) {
+        repository.deleteById(corretor.getId());
+    }
+}
